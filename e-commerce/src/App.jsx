dda7ee6ea20261329306636ce9bcaa150d171d4a
Dayvid Ramos <d.ramosm97@gmail.com>
2025-05-09 19:01:44 -0500

    fixed: Responsive mobile nav column
    description:
      * Implement function to handle show and close
        of nav bar (column) in mobile mode.

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <div id="page-wrapper"
     className="bg-slate-400 relative
     min-w-[320px]
-    min-h-[900px] h-screen">
+    min-h-[900px]">
       
       <Header />
 
@@ -24,6 +24,12 @@ function App() {
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, perferendis. Praesentium a minus consectetur expedita ducimus. Quia natus nam asperiores aliquid repellendus adipisci illum temporibus rem velit. Architecto, tenetur nihil?
 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, perferendis. Praesentium a minus consectetur expedita ducimus. Quia natus nam asperiores aliquid repellendus adipisci illum temporibus rem velit. Architecto, tenetur nihil?
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae dolor iure non beatae quos voluptates velit provident ratione fuga, quod corporis impedit deleniti sed mollitia, deserunt placeat voluptate dicta consequuntur.
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ex nobis odio nemo dolor maxime. Perspiciatis temporibus libero numquam repellat, modi earum quae beatae architecto quidem tempore, accusantium ratione facilis.
+
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum ipsa iusto officia harum nam architecto dolor sint amet consequuntur adipisci, dicta expedita iure repudiandae esse voluptatem nesciunt. Facere, iure voluptates?
 
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, perferendis. Praesentium a minus consectetur expedita ducimus. Quia natus nam asperiores aliquid repellendus adipisci illum temporibus rem velit. Architecto, tenetur nihil?
 
--- e-commerce/src/components/header/Header.jsx
+++ e-commerce/src/components/header/Header.jsx
@@ -7,26 +7,37 @@ import imageAvatar from "../../../../asset-template/images/image-avatar.png"
 
 import "./Header.css"
 
+const root = document.getElementById("root")
+
+const iconCartPath = "M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
+
+const iconClosePath = "m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+
+
+
 
 export default function Header() {
   
-  const iconCartPath = "M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z"
-
-  const iconClosePath = "m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-
   let [isNavMobColOpen,setIsNavMobColOpen] = useState(false)
 
   let [handleBackInset,setHandleBackInset] = useState("hidden")
   
   let [navColPosition,setNavColPosition] = useState("left-[-250px]")
-  
-  
+
+
   function showNavMobileColumn() {
+    root.classList.remove("overflow-auto");
+    root.classList.add("overflow-hidden");
+    
+
     setHandleBackInset("");
-    setNavColPosition("left-[0px]")
+    setNavColPosition("left-[0px]");
   }
 
   function closeNavMobileColumn() {
+    root.classList.remove("overflow-hidden");
+    root.classList.add("overflow-auto");
+
     setHandleBackInset("hidden");
     setNavColPosition("left-[-250px]");
   }
@@ -42,7 +53,8 @@ export default function Header() {
     ">
 
       <div id="background-inset" 
-      className={`bg-black opacity-75 h-screen absolute inset-0 z-20 ${handleBackInset}`}>
+      className={`bg-black opacity-75 h-screen absolute inset-0 z-20 ${handleBackInset}`}
+      onClick={closeNavMobileColumn}>
       </div>
       
       <nav id="nav-mobileColumn" 
