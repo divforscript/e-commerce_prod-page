d43aca31c7d696505c5131e66235c6084445d1ae
Dayvid Ramos <d.ramosm97@gmail.com>
2025-05-18 11:32:16 -0500

    style: product on modal v1.0 with coloring backgrounds

--- e-commerce/src/components/backInset/BackgroundInset.jsx
+++ e-commerce/src/components/backInset/BackgroundInset.jsx
@@ -1,14 +1,29 @@
 
 
+export default function BackgroundInset({ 
+  id="page-inset",
+  // isOpen=false,
+  top="top-0",
+  left="left-0",
+  zIndex="z-50",
+  align="items-center",
+  closeNavMob=()=>{},
+  childComponent={} }) {
 
-export default function BackgroundInset({bgColor="bg-black",zIndex,closeNavMob}) {
+
   return(
     <div
-        id="background-inset"
-        className={`${bgColor} opacity-75 h-screen absolute inset-0 ${zIndex}`}
-        onClick={closeNavMob}
+      id={id}
+      className={`bg-[hsl(0,0%,0%,0.75)] 
+      w-[100vw] h-[100vh] absolute inset-0
+      flex flex-col justify-center ${align}
+      ${top} ${left} ${zIndex}`}
+      // ${isOpen? "inline" : "hidden"}
+      
+      
+      onClick={closeNavMob}
     >
-
+      {childComponent}
     </div>
   );
 }
